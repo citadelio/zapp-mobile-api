eec7dddb57044ac309421435e9217a85b380a3f4
citadelio <warrakehussein@gmail.com>
2020-06-18 13:27:08 +0100

    Update

--- routes/games.js
+++ routes/games.js
@@ -4,13 +4,13 @@ const isRequestFromMobile = require("../middleware/mobilecheck");
 
 // MODELS
 const GamesModel = require('../models/Games')
+const UsersModel = require('../models/User')
 
 
 router.post('/validate-code', isRequestFromMobile, async(req, res)=>{
     const {gameCode} = req.body
     try{
         const game = await GamesModel.findOne({gameCode});
-        console.log(game);
         return res.json({
             status: game?false:true
         }) 
@@ -20,5 +20,44 @@ router.post('/validate-code', isRequestFromMobile, async(req, res)=>{
           });
       }
 })
+
+router.post('/game-data', isRequestFromMobile, async(req, res)=>{
+      const {gameCode} = req.body
+      try{
+        const game = await GamesModel.findOne({gameCode});
+        if(!game) {
+          return res.json({
+            errors: [{ msg: "Error occured" }]
+          });
+        }
+        let newPlayersDetail = [];
+        game.players.map(async player => {
+            let  userDetail = await getPlayerDetails(player);
+            newPlayersDetail.push(userDetail)
+        })
+        const gameData = {
+          ...game,
+          players:newPlayersDetail
+        }
+        console.log(gameData)
+        return res.json(gameData);
+      }catch(err){
+        return res.json({
+          errors: [{ msg: "Error occured" }]
+        });
+      }
+})
+
+const getPlayerDetails = async player=>{
+      try{
+          const user = await UsersModel.findById(player.playerId);
+          return user;
+      }catch(err){
+        return res.json({
+          errors: [{ msg: "Error occured" }]
+        });
+      }
+}
+
 
 module.exports = router;
