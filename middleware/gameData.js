3ef97e52df748f2566cf35fc8f35a7a40d647717
citadelio <warrakehussein@gmail.com>
2020-06-21 22:47:35 +0100

    Development Update

--- middleware/gameData.js
+++ middleware/gameData.js
@@ -6,6 +6,7 @@ module.exports = [
         name:"Fastest Finger",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592729082/sprinter_cp9ni0.png",
         description:"This is a fastest fingers game. On Android at most three buttons can be specified. Android has a concept of a neutral, negative and a positive button. Optionally provide a list of buttons. Tapping any button will fire the respective onPress callback and dismiss the alert. By default, the only button will be an 'OK' button.",
+        stake:200,
         available:true
     },
     {
@@ -13,6 +14,7 @@ module.exports = [
         name:"Anagram",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592727210/gameboy_cgwpct.png",
         description:"This is an API that works both on Android and iOS and can show static alerts. To show an alert that prompts the user to enter some information, see AlertIOS; entering text in an alert is common on iOS only. Launches an alert dialog with the specified title and message.",
+        stake:200,
         available:false
     },
     {
@@ -20,6 +22,7 @@ module.exports = [
         name:"Word Search",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592727211/social_qgr9vg.png",
         description:"A React component for displaying different types of images, including network images, static resources, temporary local images, and images from local disk, such as the camera roll. This example shows fetching and displaying an image from local storage as well as one from network and even from data provided in the 'data:' uri scheme",
+        stake:200,
         available:false
     },
 ]
