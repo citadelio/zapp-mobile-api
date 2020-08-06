1af0130f8ef6dc4a47991bec1953e0f1ed5ea7fd
citadelio <warrakehussein@gmail.com>
2020-08-06 17:15:52 +0100

    Update

--- middleware/gameData.js
+++ middleware/gameData.js
@@ -5,7 +5,7 @@ module.exports = [
         key:"1",
         name:"Fastest Finger",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592729082/sprinter_cp9ni0.png",
-        description:"This is a fastest fingers game. On Android at most three buttons can be specified. Android has a concept of a neutral, negative and a positive button. Optionally provide a list of buttons. Tapping any button will fire the respective onPress callback and dismiss the alert. By default, the only button will be an 'OK' button.",
+        description:"Challenge your friends to type fast!. You will be presented with words at intervals, first person to type a word gets a point. There are 15 words in total. The winner at the end of 15 words wins the prize money. Type as fast ad accurate as possible.",
         stake:200,
         available:true
     },
@@ -13,7 +13,7 @@ module.exports = [
         key:"2",
         name:"Anagram",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592727210/gameboy_cgwpct.png",
-        description:"This is an API that works both on Android and iOS and can show static alerts. To show an alert that prompts the user to enter some information, see AlertIOS; entering text in an alert is common on iOS only. Launches an alert dialog with the specified title and message.",
+        description:"Anagrams are words or phrases you spell by rearranging the letters of another word or phrase. You will be shown words at intervals and are expected to rearrange the word to form another word.",
         stake:200,
         available:false
     },
@@ -21,7 +21,7 @@ module.exports = [
         key:"3",
         name:"Word Search",
         photo:"https://res.cloudinary.com/citadel/image/upload/v1592727211/social_qgr9vg.png",
-        description:"A React component for displaying different types of images, including network images, static resources, temporary local images, and images from local disk, such as the camera roll. This example shows fetching and displaying an image from local storage as well as one from network and even from data provided in the 'data:' uri scheme",
+        description:"Word Search is a fun puzzle game. You will be presented with a tile of different letters and you are expected to form words from the letters in the tiles.",
         stake:200,
         available:false
     },
